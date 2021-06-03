420fddc719b2cae559730a67596c5713c36a0a85
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2021-06-03 13:06:12 +0100

    Add files via upload

---
+++ krakon.js
@@ -0,0 +1,20 @@
+function init(){
+document.getElementById("input-error").style.display = "none";}
+function exportcode() {
+    inputerror = document.getElementById("input-error");
+    inputerror.style.display = "initial";
+    inputerror.innerHTML = "Error: Export is disabled.";
+}
+
+function importcode(){
+    httpGet("https://google.com")
+    document.getElementById("raw-code-edit").innerHTML = document.getElementById("raw-code-area").value;
+}
+
+function httpGet(theUrl, data=null)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send(data);
+    return xmlHttp.responseText;
+}
