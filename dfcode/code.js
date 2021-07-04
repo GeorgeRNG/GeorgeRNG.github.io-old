43e28d94c0e207a2ec57cb35ffa6e69d52c522f2
George RNG <>
2021-07-04 14:24:39 +0100

    Render and simple select

--- dfcode/code.js
+++ dfcode/code.js
@@ -37,6 +37,16 @@ function rendblocks(){
     document.getElementById("rawdecoded").innerHTML = code
 }
 
+function selectblock(clickedobj){
+    selected = Number(clickedobj.target.id.replace("block-",""))
+    final = document.createElement("div")
+    p = document.createElement("p")
+    p.innerHTML = "Block " + String(selected)
+    final.appendChild(p)
+    document.getElementById("blockinfo").innerHTML = ""
+    document.getElementById("blockinfo").appendChild(final)
+}
+
 function imgelement(link, classes = []){
     img = document.createElement("img");
     img.src = link
@@ -49,11 +59,27 @@ function rendblocks(){
     document.getElementById("code-list").innerHTML = ""
     JSON.parse(code)["blocks"].forEach((block, index) => {
         if(block["id"] == "block"){
-            img = (imgelement("images/blocks/" + block["block"] + ".png", ["block"]));
+            img = (imgelement("images/normal/" + block["block"] + ".png", ["block"]));
             img.onclick = function(index){selectblock(index)};
             img.classList.add("block")
             img.id = "block-" + String(index);
             document.getElementById("code-list").appendChild(img);
         }
+        if(block["id"] == "bracket"){
+            if(block["type"] != "norm"){
+                img = (imgelement("images/brackets/" + block["direct"] + "s.png", ["block"]));
+                img.onclick = function(index){selectblock(index)};
+                img.classList.add("block")
+                img.id = "block-" + String(index);
+                document.getElementById("code-list").appendChild(img);
+            }else{
+                img = (imgelement("images/brackets/" + block["direct"] + ".png", ["block"]));
+                img.onclick = function(index){selectblock(index)};
+                img.classList.add("block")
+                img.id = "block-" + String(index);
+                document.getElementById("code-list").appendChild(img);
+            }
+        }
+        
     });
 }
