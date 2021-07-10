a1340ede22b5c6001b78a137d88adf797559aa7d
George RNG <>
2021-07-10 07:22:27 +0100

    made a funcion

--- dfcode/code.js
+++ dfcode/code.js
@@ -50,99 +50,8 @@ function rendblocks(){
     })
 }
 
-function selectblock(clickedobj){
-    document.getElementById("blockinfo").innerHTML = ""
-    selected = Number(clickedobj.target.id.replace("block-",""))
-    var final = document.createElement("div")
-    p = document.createElement("span")
-    p.innerHTML = "Block " + String(selected)
-    final.appendChild(p)
-    var parsed = JSON.parse(code)
-    var block = parsed["blocks"][selected]
-    if(block["id"] == "block"){
-        var input = document.createElement("span")
-        input.innerHTML = "<br/>Action:"
-        final.appendChild(input)
-        var input = document.createElement("input")
-        input.value = block["action"]
-        input.id = "action"
-        input.onchange = () => {
-            var x = parsed
-            x["blocks"][selected]["action"] = document.getElementById("action").value
-            code = JSON.stringify(x);
-            rendblocks()
-        }
-        final.appendChild(input)
-        var input = document.createElement("span")
-        input.innerHTML = "<br/>Selection:"
-        final.appendChild(input)
-        var input = document.createElement("input")
-        input.id = "target"
-        if(block["target"] != undefined)
-            {input.value = block["target"]}
-        input.onchange = () => {
-                var x = parsed
-                x["blocks"][selected]["target"] = document.getElementById("target").value
-                code = JSON.stringify(x);
-                rendblocks()
-            }
-        final.appendChild(input)
-        var input = document.createElement("span")
-        input.innerHTML = "<br/>NOT:"
-        final.appendChild(input)
-        var input = document.createElement("input")
-        input.id = "inverted"
-        if(block["inverted"] != undefined)
-            {input.value = block["inverted"]}
-        input.onchange = () => {
-                var x = parsed
-                x["blocks"][selected]["inverted"] = document.getElementById("inverted").value
-                code = JSON.stringify(x);
-                rendblocks()
-            }
-        final.appendChild(input)
-    }
-    if(block["id"] == "bracket"){
-        final.appendChild(document.createElement("br"))
-        var input = document.createElement("select")
-        input.innerHTML = '<option value="open">Opening Bracket</option><option value="close">Closing Bracket</option>'
-        input.value = parsed["blocks"][selected]["direct"]
-        input.id = "direct"
-        input.onchange = () => {
-            var x = parsed
-            x["blocks"][selected]["direct"] = document.getElementById("direct").value
-            code = JSON.stringify(x);
-            rendblocks()
-        }
-        final.appendChild(input)
-        final.appendChild(document.createElement("br"))
-        var input = document.createElement("input")
-        input.type = "checkbox"
-        if(parsed["blocks"][selected]["type"] == "repeat"){
-            input.checked = true
-        }
-        input.id = "sticky"
-        input.onclick = () => {
-            var x = parsed
-                x["blocks"][selected]["type"] = document.getElementById("sticky").checked ?  "repeat" : "norm"
-                code = JSON.stringify(x);
-                rendblocks()}
-        final.appendChild(input)
-        var input = document.createElement("label")
-        input.innerHTML = "Sticky"
-        input.for = "sticky"
-        final.appendChild(input)
-    }
-    document.getElementById("blockinfo").appendChild(final)
-}
-
-function selectblock(clickedobj){
-    var selected = Number(clickedobj.target.id.replace("block-",""))
-    document.getElementById("blockinfo").innerHTML = "<p>Block " + selected + "</p>"
-    var parsed = JSON.parse(code)
-    var block = parsed["blocks"][selected]
-    if(block["id"] == "block"){for(var key in block){
-        if(!(key == "args" || key == "id" || key == "block")){
+function oneblock(key,block){
+    if(!(key == "args" || key == "id" || key == "block")){
         var obj = document.createElement("label")
         obj.innerHTML = key + " "
         obj.setAttribute("type","text")
@@ -163,7 +72,17 @@ function selectblock(clickedobj){
             rendblocks()
         }
         document.getElementById("blockinfo").appendChild(obj)
-        document.getElementById("blockinfo").appendChild(document.createElement("br"))}
+        document.getElementById("blockinfo").appendChild(document.createElement("br"))
+    }
+}
+
+function selectblock(clickedobj){
+    var selected = Number(clickedobj.target.id.replace("block-",""))
+    document.getElementById("blockinfo").innerHTML = "<p>Block " + selected + "</p>"
+    var parsed = JSON.parse(code)
+    var block = parsed["blocks"][selected]
+    if(block["id"] == "block"){for(var key in block){
+        oneblock(key,block)
     }}
     if(block["id"] == "bracket"){
         var final = document.createElement("div")
@@ -198,6 +117,8 @@ function selectblock(clickedobj){
         document.getElementById("blockinfo").appendChild(final)
     }
 }
+
+
 
 function copy() {
     var copyText = document.getElementById("encodedoutput");
